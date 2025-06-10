cee8194d0fa360e029b0d4172b34dbe426817779
Ammar Ahmed <ammarahmed132004@outlook.com>
2025-06-10 16:36:22 +0300

    ED

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,2 +1,13 @@
+const togglePassword = document.getElementById("togglePassword");
+const password = document.getElementById("password");
 
+togglePassword.addEventListener("click", () => {
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
 
+  togglePassword.setAttribute(
+    "name",
+    type === "password" ? "eye-outline" : "eye-off-outline"
+  );
+});
